9d0430a6213bca4b25a31d910ddb002347e1dc6a
R0flyan <145720242+R0flyan@users.noreply.github.com>
2023-10-16 12:56:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+answer = 0
+
+@app.post("/calculator")
+#объявляем основную функцию, которая будет выполнять роль калькулятора
+def calculator(first_number: float, second_number: float, operator: str):
+    #Прописываем проверки на введенную команду оператора для вычислений.
+    #При выполнении проверки, прописываем инструкции для вычисления, результат записываем в переменную ответа
+    #и возвращаем из функции значение ответа.
+    if operator == "+":
+        answer = first_number + second_number
+        return answer
+    if operator == "-":
+        answer = first_number - second_number
+        return answer
+    if operator == "*":
+        answer = first_number * second_number
+        return answer
+    if operator == "/":
+        answer = first_number / second_number
+        return answer
+    # if second_number == 0:
+    #     return {"ошибка. делить на 0 нельзя"}
+    else:
+        return {"error. Проверьте введенные значения"}
